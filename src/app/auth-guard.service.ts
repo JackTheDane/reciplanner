f391d12732d9a61bc94cbf29647810c4eb4261e2
jackthedane <fader4ever@hotmail.com>
2018-09-21 07:46:35 +0200

    Admin routing added, authguard wip

--- src/app/auth-guard.service.ts
+++ src/app/auth-guard.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
+import { CanActivate } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuardService {
-
-  constructor() { }
+export class AuthGuardService implements CanActivate {
+  canActivate() {
+    console.log('AuthGuard#canActivate called');
+    return true;
+  }
 }
